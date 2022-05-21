e92e38e79a7f6011a12e51fe824199ca550623d0
Tal Livneh <talivneh8@gmail.com>
2022-05-21 15:30:30 +0300

    feat: adds ItemManager module skeltone

---
+++ src/ex2/ItemManager.js
@@ -0,0 +1,11 @@
+export default class ItemManager {
+  constructor() {
+    this.API_BASE = "https://pokeapi.co/api/v2";
+  }
+
+  async addPokemone() {}
+
+  async removePokemone() {}
+
+  async getAllPokemons() {}
+}
