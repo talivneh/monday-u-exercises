f4c91b8f1737a44a808fc542a3fdbed26e7045ee
Tal Livneh <talivneh8@gmail.com>
2022-05-22 15:17:18 +0300

    refactor: change view to be a class with methods

---
+++ src/ex2/View.js
@@ -0,0 +1,161 @@
+export default class View {
+  constructor() {
+    this.toDoInput = document.querySelector("input");
+    this.listContainer = document.querySelector("ul");
+    this.alertBox = document.querySelector(".alert");
+    this.alertBoxText = document.querySelector(".alert-innet-text");
+  }
+
+  init() {
+    const aletBoxCloseBtn = document.getElementById("alert-close-btn");
+    aletBoxCloseBtn.addEventListener("click", () => {
+      this.closeAlertBox();
+    });
+
+    const deleteAllBtn = document.getElementById("clear-all");
+    deleteAllBtn.addEventListener("click", () => {
+      this.listContainer.childNodes.forEach((item) => {
+        this.deleteItem(item);
+      });
+    });
+  }
+
+  get _inputText() {
+    return this.toDoInput.value;
+  }
+
+  _resetInput() {
+    this.toDoInput.value = "";
+  }
+
+  getElement(selector) {
+    const element = document.querySelector(selector);
+    return element;
+  }
+
+  handleAddBtnClick(handler) {
+    if (this._inputText) {
+      this.createNewToDoElement(handler);
+    } else {
+      this.alert(null);
+    }
+  }
+
+  async createNewToDoElement(handler) {
+    const text = this._inputText;
+    this._resetInput();
+
+    const time = new Date();
+    const item = {
+      text,
+      time,
+      complete: false,
+      checkTime: null,
+      toggleComplete() {
+        this.complete = !this.complete;
+        this.checkTime = !this.checkTime
+          ? new Date().toLocaleDateString()
+          : null;
+      },
+    };
+
+    item.text = await handler(item);
+
+    const listItem = document.createElement("li");
+
+    const listItemCheckbox = document.createElement("input");
+    listItemCheckbox.setAttribute("type", "checkbox");
+    listItemCheckbox.className = "todo-item-checkbox";
+
+    listItemCheckbox.addEventListener("change", () => {
+      item.toggleComplete();
+    });
+
+    const listItemText = document.createElement("span");
+    listItemText.className = "todo-item";
+    listItemText.innerText = item.text;
+    listItemText.addEventListener("click", () => {
+      this.alert(this.createDetails(item));
+    });
+
+    const listItemRemoveBtnContainer = document.createElement("span");
+    listItemRemoveBtnContainer.className = "btn-container";
+
+    const listItemRemoveBtn = document.createElement("button");
+    listItemRemoveBtn.className = "remove-btn";
+    listItemRemoveBtn.innerHTML = `<i class="fas fa-trash"></i>`;
+    listItemRemoveBtn.addEventListener("click", () => {
+      this.deleteItem(listItem);
+    });
+
+    listItemRemoveBtnContainer.appendChild(listItemRemoveBtn);
+    listItem.appendChild(listItemCheckbox);
+    listItem.appendChild(listItemText);
+    listItem.appendChild(listItemRemoveBtnContainer);
+
+    this.listContainer.appendChild(listItem);
+    this.updateTasksNum();
+  }
+
+  deleteItem(elementToDelte) {
+    const container = this.listContainer;
+    elementToDelte.classList.add("leave");
+
+    const updateTasksNum = this.updateTasksNum;
+
+    setTimeout(() => {
+      container.removeChild(elementToDelte);
+      updateTasksNum.call(this);
+    }, 800);
+  }
+
+  updateTasksNum() {
+    const footersItemsNumberSpan = document.getElementById("tasks-number");
+    footersItemsNumberSpan.innerText =
+      this.listContainer.childNodes.length.toString();
+    this.toggleEmptyView.call(this);
+  }
+
+  toggleEmptyView() {
+    const listPlaceHolder = document.querySelector(".empty-list");
+    const footer = document.querySelector("footer");
+
+    if (this.getElement("li")) {
+      listPlaceHolder.classList.add("hide");
+      footer.classList.remove("hide");
+    } else {
+      footer.classList.add("hide");
+      setTimeout(() => {
+        listPlaceHolder.classList.remove("hide");
+      }, 100);
+    }
+  }
+
+  closeAlertBox() {
+    this.alertBox.classList.remove("show", "warning", "info");
+  }
+
+  createDetails(item) {
+    console.log(item.complete);
+    return `<span><span>To do:</span> ${item.text}</span>
+    <span><span>Creation-date:</span> ${item.time.toLocaleDateString()}</span>
+    ${
+      item.complete
+        ? `<span class="done"><span>Done at:</span> ` +
+          item.checkTime +
+          `</span>`
+        : ``
+    } `;
+  }
+
+  alert(alert) {
+    if (!alert) {
+      this.alertBox.classList.add("show", "warning");
+      this.alertBoxText.innerText =
+        "please write some text before adding new ToDo";
+    } else {
+      this.alertBox.classList.add("show", "info");
+      this.alertBoxText.innerHTML = alert;
+    }
+  }
+}
--- src/ex2/main.js
+++ src/ex2/main.js
@@ -1,10 +1,27 @@
-// Implement the `Main` class here
+import View from "./View.js";
+import ItemManager from "./ItemManager.js";
 
+class Main {
+  constructor() {
+    this.view = new View();
+    this.itemManager = new ItemManager();
+  }
+
+  init() {
+    this.view.init();
+    const addBtn = document.getElementById("add-todo");
+    addBtn.addEventListener("click", () => {
+      this.view.handleAddBtnClick(
+        this.itemManager.addItem.bind(this.itemManager)
+      );
+    });
+  }
+}
 
 const main = new Main();
 
 document.addEventListener("DOMContentLoaded", function () {
-    // you should create an `init` method in your class
-    // the method should add the event listener to your "add" button
-    main.init();
+  // you should create an `init` method in your class
+  // the method should add the event listener to your "add" button
+  main.init();
 });
